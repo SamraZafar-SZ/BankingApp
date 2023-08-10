832889f2380f692ba7599cc235b4a6ac68a4cd1c
Ali Hassan <zaidialisyed9@gmail.com>
2023-08-10 18:34:19 +0500

    Refactoring of Code2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_4/widgets/home.dart';
 import 'package:flutter_application_4/widgets/screens/LoginSignup.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
